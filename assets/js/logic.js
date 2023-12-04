49e78b66ef549f4f1eb52912f60e38771249d814
2akia <2akssultana@gmail.com>
2023-12-04 00:01:46 +0000

    added the answer function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,35 @@
 
-questions.length
+var currentIndex = 0;
 
-for (var i= 0; i< questions.length; i++) {
+var timeLeft = 75;
+var timeEl = document.getElementById("time");
+function startTimer() {
+    setInterval(function () {
+        timeEl.textContent = timeLeft;
+        timeLeft--;
+    }, 1000)
+};
 
+function answerClick(event) {
+    var clickAnswer = event.target.textContent;
 
-    
+    var correctAnswer = questions[currentIndex].correct;
 
+    if (clickAnswer == correctAnswer) {
+        alert("correct")
+    } else {
+        alert("wrong")
+    }
+
+    if (currentIndex < questions.length - 1) {
+        currentIndex++;
+        displayQuestion();
+    } else {
+        var questionContainer = document.getElementById("questions");
+        questionContainer.classList.add("hide");
+        var endScreen = document.getElementById("end-screen");
+        endScreen.classList.remove("hide");
+
+    }
 }
-
 
